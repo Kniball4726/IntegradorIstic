cac067c6d78dd4341b5215fd1aa404826c1186f0
Gregory Rodriguez <gregory@RedKali.glrd4726ml@gmail.com>
2026-06-16 22:02:38 -0300

    nuevos cambios en linux

--- src/Persistencia/Pedidos.py
+++ src/Persistencia/Pedidos.py
@@ -7,6 +7,7 @@ class Pedidos:
         self.producto = producto
         self.cantidad = cantidad
         self.estado = estado
-
+    """    El método __str__ se encarga de mostrar toda la información del pedido de manera legible, incluyendo el ID del pedido, DNI del cliente, producto solicitado, cantidad y estado del pedido. Esto facilita la visualización de los detalles del pedido cuando se imprime el objeto Pedidos.
+    """
     def __str__(self):
         return f"ID Pedido: {self.idPedido}, DNI Cliente: {self.dniCliente}, Producto: {self.producto}, Cantidad: {self.cantidad}, Estado: {self.estado}"
